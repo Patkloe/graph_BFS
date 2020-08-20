b5b9b5e5e1f7a48f9dd48599af3ea3707260f5ce
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-20 08:17:41 -0500

    Update parcours_bfs_dfs.js

--- parcours_bfs_dfs.js
+++ parcours_bfs_dfs.js
@@ -10,31 +10,19 @@ class Graph{
   this.listadj.get(v).push(w);
   this.listadj.get(w).push(v);
  }
- printGraph() 
- { 
-    // get all the vertices 
-    var get_keys = this.listadj.keys(); 
-  
-    // iterate over the vertices 
-    for (var i of get_keys)  
-    { 
-        // great the corresponding adjacency list 
-        // for the vertex 
-        var get_values = this.listadj.get(i); 
-        var conc = ""; 
-  
-        // iterate over the adjacency list 
-        // concatenate the values into a string 
-        for (var j of get_values) 
-            conc += j + " "; 
-  
-        // print the vertex and its adjacency list 
-        console.log(i + " -> " + conc); 
-     } 
- } 
- parcours_BFS(startpoint){
+ printgraph(){
+  var getkeys = this.listadj.keys();
+  for(var i of getkeys){
+     var conc = " ";
+     var listsomadj = this.listadj.get(i);
+     for(var j of listsomadj)
+      conc = conc + j;
+   console.log(i + "->" + conc);
+  }
+ } // fin printgraph
+ parcours_BFS(startingpt){
   function Queue(){
-   this.elements =[];
+   this.elements = [];
   }
   Queue.prototype.enqueue = function(x){
    return this.elements.push(x);
@@ -46,67 +34,55 @@ class Graph{
    return this.elements.length;
   }
   var dejavu = [];
-   for(i = 0; i < this.nbresommets; i++)
-       dejavu[i] = false;
-       var filesommet =  new Queue();
-       dejavu[startpoint] = true;
-       filesommet.enqueue(startpoint);
-       while(filesommet.length() > 0){
-        var sommetout = filesommet.dequeue();
-        console.log("sommet sorti de la file :" + " " + sommetout);
-        var listsommet = this.listadj.get(sommetout);
-         for(var j in listsommet){
-          if(!dejavu[listsommet[j]]){
-              dejavu[listsommet[j]] = true;
-              filesommet.enqueue(listsommet[j]);
-          }// fin if
-         } // fin for
-       } // fin while
- } // fin parcours_DFS
+  for(var i = 0; i < this.nbresommets; i++)
+   dejavu[i] = false;
+   var filesommet = new Queue();
+   dejavu[startingpt] = true;
+   filesommet.enqueue(startingpt);
+   while(filesommet.length() > 0){
+    var sommetout = filesommet.dequeue();
+    console.log(" sommet sorti tete de file : " + " " + sommetout);
+    var listsommet = this.listadj.get(sommetout);
+     for(var j in listsommet){
+      if(!dejavu[listsommet[j]]){
+         dejavu[listsommet[j]] = true;
+         filesommet.enqueue(listsommet[j]);
+      }
+     } // fin for
+   }// fin while
+
+ }// fin parcours_BFS
  parcours_DFS(startingpt){
   var dejavu = [];
   for(var i = 0; i < this.nbresommets; i++)
-   dejavu[i] = false;
-  this.DFSproc(startingpt,dejavu);
- } // fin parcours_DFS
- DFSproc(sommet,dejavu){
+      dejavu[i] = false;
+      this.DFSutil(startingpt,dejavu);
+ }// fin parcours_DFS
+ DFSutil(sommet,dejavu){
   dejavu[sommet] = true;
-  console.log("sommet rencontre :" + " " +sommet);
-  var listsommet = this.listadj.get(sommet);
-  for(var j in listsommet){
-   if(!dejavu[listsommet[j]]) 
-     this.DFSproc(listsommet[j],dejavu);
+  console.log("sommet parcouru : " + " " + sommet);
+  var sommetvis = this.listadj.get(sommet);
+  for(var i in sommetvis){
+   if(!dejavu[sommetvis[i]])
+     this.DFSutil(sommetvis[i],dejavu);
   }
  }
-}// fin class Graph
-var g = new Graph(6); 
-var vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ]; 
-  
-// adding vertices 
-for (var i = 0; i < vertices.length; i++) { 
-    g.addsommet(vertices[i]); 
-} 
-  
-// adding edges 
-g.addchemin('A', 'B'); 
-g.addchemin('A', 'D'); 
-g.addchemin('A', 'E'); 
-g.addchemin('B', 'C'); 
-g.addchemin('D', 'E'); 
-g.addchemin('E', 'F'); 
-g.addchemin('E', 'C'); 
-g.addchemin('C', 'F'); 
-  
-// prints all vertex and 
-// its adjacency list 
-// A -> B D E 
-// B -> A C 
-// C -> B E F 
-// D -> A E 
-// E -> A D F C 
-// F -> E C 
-g.printGraph(); 
-console.log("BFS"); 
-g.parcours_BFS('A');
-console.log("DFS");  
-g.parcours_DFS('A');
+} // fin class Graph
+var test = new Graph(7)
+var tab = ['A','B','C','D','E','F','G'];
+for( var i = 0; i < tab.length; i++)
+  test.addsommet(tab[i]);
+   test.addchemin('A','B');
+   test.addchemin('A','C');
+   test.addchemin('A','D');
+   test.addchemin('B','C');
+   test.addchemin('B','E');
+   test.addchemin('D','G');
+   test.addchemin('C','F');
+   test.addchemin('C','G');
+   test.addchemin('E','F');
+   test.printgraph();
+   console.log("parcours en largeur BFS");
+   test.parcours_BFS('A');
+   console.log("parcours en profondeur DFS");
+   test.parcours_DFS('A');
