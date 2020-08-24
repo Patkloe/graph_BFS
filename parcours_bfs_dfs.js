007d18e0e8ac855742af77b08fe4622de9e5e447
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-24 11:37:16 -0500

    Update parcours_bfs_dfs.js

--- parcours_bfs_dfs.js
+++ parcours_bfs_dfs.js
@@ -12,14 +12,14 @@ class Graph{
  }
  printgraph(){
   var getkeys = this.listadj.keys();
-  for(var i of getkeys){
-     var conc = " ";
-     var listsomadj = this.listadj.get(i);
-     for(var j of listsomadj)
-      conc = conc + j;
-   console.log(i + "->" + conc);
-  }
- } // fin printgraph
+   for(var i of getkeys){
+    var conc = " ";
+    var listsommet = this.listadj.get(i)
+     for(var j of listsommet)
+       conc = conc + j;
+    console.log(i + ""+"->"+conc);
+   }
+ }// fin printgraph
  parcours_BFS(startingpt){
   function Queue(){
    this.elements = [];
@@ -35,62 +35,63 @@ class Graph{
   }
   var dejavu = [];
   for(var i = 0; i < this.nbresommets; i++)
-   dejavu[i] = false;
-   var filesommet = new Queue();
-   dejavu[startingpt] = true;
-   filesommet.enqueue(startingpt);
-   while(filesommet.length() > 0){
-    var sommetout = filesommet.dequeue();
-    console.log(" sommet sorti tete de file : " + " " + sommetout);
-    var listsommet = this.listadj.get(sommetout);
-     for(var j in listsommet){
-      if(!dejavu[listsommet[j]]){
-         dejavu[listsommet[j]] = true;
-         filesommet.enqueue(listsommet[j]);
-      }
-     } // fin for
-   }// fin while
-
+      dejavu[i] = false;
+  var filesommet = new Queue();
+  dejavu[startingpt] = true;
+  filesommet.enqueue(startingpt);
+  while(filesommet.length() > 0){
+   var sommetout = filesommet.dequeue();
+   console.log("sommet visite :" + " " + sommetout);
+   var listsommet = this.listadj.get(sommetout);
+   for(var j in listsommet){
+     if(!dejavu[listsommet[j]]){
+        dejavu[listsommet[j]]= true;
+        filesommet.enqueue(listsommet[j]); 
+     }// fin if
+   }// fin for
+  }// fin while
  }// fin parcours_BFS
  parcours_DFS(startingpt){
   var dejavu = [];
   for(var i = 0; i < this.nbresommets; i++)
-      dejavu[i] = false;
-      this.DFSutil(startingpt,dejavu);
+     dejavu[i] = false;
+     this.DFSutil(startingpt,dejavu);     
  }// fin parcours_DFS
  DFSutil(sommet,dejavu){
   dejavu[sommet] = true;
-  console.log("sommet parcouru : " + " " + sommet);
-  var sommetvis = this.listadj.get(sommet);
-  for(var i in sommetvis){
-   if(!dejavu[sommetvis[i]])
-     this.DFSutil(sommetvis[i],dejavu);
+  console.log("sommet visite : " + " " + sommet);
+  var sommetlist = this.listadj.get(sommet);
+  for(var i in sommetlist){
+   if(!dejavu[sommetlist[i]])
+     this.DFSutil(sommetlist[i],dejavu);
   }
- }
-} // fin class Graph
-var test = new Graph(7)
-var tab = ['A','B','C','D','E','G','H','I','J','H'];
-for( var i = 0; i < tab.length; i++)
+ }//fin DFSutil
+}//fin class Graph
+var tab = ['A','B','C','D','E','F','G','H','I','J'];
+var test = new Graph(10);
+for(var i =0; i < tab.length; i++)
   test.addsommet(tab[i]);
-   test.addchemin('A','B');
-    test.addchemin('A','C');
-    test.addchemin('A','D');
-    test.addchemin('B','E');
-    test.addchemin('C','I');
-    test.addchemin('C','F');
-    test.addchemin('D','H');
-    test.addchemin('E','I');
-    test.addchemin('E','F');
-    test.addchemin('F','H');
-    test.addchemin('F','J');
-    test.addchemin('H','J');
-    test.addchemin('I','G');
-    test.addchemin('I','H');
-    test.addchemin('G','H');
-    test.addchemin('G','J');
-    test.addchemin('J','H');
-   test.printgraph();
-   console.log("parcours en largeur BFS");
-   test.parcours_BFS('A');
-   console.log("parcours en profondeur DFS");
-   test.parcours_DFS('A');
+  test.addchemin('A','B');
+  test.addchemin('A','C');
+  test.addchemin('A','D');
+  test.addchemin('B','E');
+  test.addchemin('B','C');
+  //test.addchemin('A','B');
+  test.addchemin('C','F');
+  test.addchemin('C','G');
+  test.addchemin('E','F');
+  //test.addchemin('C','G');
+  //test.addchemin('G','F');
+  test.addchemin('G','H');
+  test.addchemin('G','F');
+  test.addchemin('G','J');
+  test.addchemin('G','D');
+  test.addchemin('D','J');
+  test.addchemin('I','J');
+  test.addchemin('H','I');
+  test.addchemin('H','F');
+  test.printgraph();
+  console.log("Parcours en largeur, BFS :");
+  test.parcours_BFS('A');
+  console.log("Parcours en profondeur, DFS :");
+  test.parcours_DFS('A');
